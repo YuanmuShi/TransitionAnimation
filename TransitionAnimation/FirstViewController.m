74e14205a12078b726756a30cea0c150af092458
Jeffrey <jeffrey@JeffreydeMBP.lan>
2015-11-04 23:38:00 +0800

    Change first vc title name

--- TransitionAnimation/FirstViewController.m
+++ TransitionAnimation/FirstViewController.m
@@ -22,7 +22,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.title = @"First";
+    self.title = @"First ViewController";
     
     self.navigationController.delegate = self;
 }
